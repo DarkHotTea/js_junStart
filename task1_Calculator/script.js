4226f7e6802f645d7d78b03b8be027f60891edb2
DarkHotTea <105120676+DarkHotTea@users.noreply.github.com>
2025-08-09 19:47:12 +0300

    testing my own checking through regular expression

--- task1_Calculator/script.js
+++ task1_Calculator/script.js
@@ -1,31 +1,46 @@
 // Creating of a simple calculator
 
-alert("please, enter the nums and operation!");
+// alert("please, enter the nums and operation!");
 
-let firstNum = Number(prompt("First num:"));
+let firstNum = "-1223343333243433.1334234-4";
+let cheking = /^\-?[0-9]*\.?[0-9]*$/;
+
+console.log(cheking.test(firstNum));
+
+
+/* let checkFirstNum = (firstNum) => {
+  firstNum = Number(prompt("First num:"));
+  if (!isNaN(firstNum)) {
+    alert("Enter the num!!");
+    return checkFirstNum();
+  }
+};
 let secondNum = Number(prompt("Second num:"));
 let operation = prompt("And the operation...");
 let answer;
 
-switch (operation) {
-  case "+":
-    answer = firstNum + secondNum;
-    break;
-  case "-":
-    answer = firstNum - secondNum;
-    break;
-  case "*":
-    answer = firstNum * secondNum;
-    break;
-  case "/":
-    answer = firstNum / secondNum;
-    break;
-  case "%":
-    answer = firstNum % secondNum;
-    break;
-  default:
-    alert("Please, don't type a shit");
-    break;
-}
+function calc(operation, firstNum, secondNum) {
+  switch (operation) {
+    case "+":
+      answer = firstNum + secondNum;
+      break;
+    case "-":
+      answer = firstNum - secondNum;
+      break;
+    case "*":
+      answer = firstNum * secondNum;
+      break;
+    case "/":
+      answer = firstNum / secondNum;
+      break;
+    case "%":
+      answer = firstNum % secondNum;
+      break;
+    default:
+      alert("Please, don't type a shit");
+      break;
+  }
 
-answer == undefined ? alert("💩") : alert(`Your answer is ${answer}`)
+  return answer == undefined ? alert("💩") : alert(`Your answer is ${answer}`);
+}
+ */
